cf21ff359ac03b8339448b09e2d040eabb081336
AdebolaShelby <deby29050@gmail.com>
2024-08-25 18:17:47 -0700

    added updated query files, table schema and erd

---
+++ Employee_SQL/sqlquery/queries-updated.sql
@@ -0,0 +1,90 @@
+---confirm rows count---
+SELECT COUNT(*) FROM employees;
+
+--List the employee number, last name, first name, sex, and salary of each employee.--
+
+SELECT  
+	e.emp_no AS employee_number,
+	e.last_name,
+	e.first_name, 
+	e.sex,
+	s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+ORDER BY first_name;
+
+
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+
+SELECT 
+	e.first_name, 
+	e.last_name,
+	e.hire_date
+FROM employees e
+WHERE e.hire_date LIKE '%1986'
+	ORDER BY e.hire_date;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+SELECT 
+	dem.dept_no, 
+	d.dept_name, 
+	dem.emp_no, 
+	e.last_name, 
+	e.first_name
+FROM dept_manager dem
+JOIN departments d ON dem.dept_no = d.dept_no
+JOIN employees e ON dem.emp_no = e.emp_no;
+
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+SELECT  
+    dem.dept_no AS department_number,
+    e.emp_no AS employee_number,
+	e.last_name,
+    e.first_name,
+    d.dept_name
+FROM employees e
+JOIN dept_emp dem ON e.emp_no = dem.emp_no
+JOIN departments d ON dem.dept_no = d.dept_no
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT
+	e.first_name,
+	e.last_name,
+	e.sex
+FROM employees e
+where e.first_name = 'Hercules' AND last_name LIKE 'B%'
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT * 
+	FROM departments d;
+-- The department number of Sales department is d007
+SELECT 
+	e.emp_no, 
+	e.last_name, 
+	e.first_name, 
+	d.dept_name
+FROM employees e
+JOIN dept_emp dem ON e.emp_no = dem.emp_no
+JOIN departments d ON dem.dept_no = d.dept_no
+WHERE d.dept_no = 'd007';
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT 
+	e.emp_no, 
+	e.last_name, 
+	e.first_name, 
+	d.dept_name
+FROM employees e
+JOIN dept_emp dem ON e.emp_no = dem.emp_no
+JOIN departments d ON dem.dept_no = d.dept_no
+WHERE d.dept_no = 'd005' or d.dept_no = 'd007';
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
--- Employee_SQL/sqlquery/table-schema.sql
+++ Employee_SQL/sqlquery/table-schema.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE "departments" (
     "dept_no" varchar(20)   NOT NULL,
     "dept_name" varchar(50)   NOT NULL,
@@ -10,11 +9,11 @@ CREATE TABLE "departments" (
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" varchar(10)   NOT NULL,
-    "birth_date" date   NOT NULL,
+    "birth_date" varchar(20),
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
     "sex" varchar(1)   NOT NULL,
-    "hire_date" date   NULL,
+    "hire_date" varchar(20),
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -23,32 +22,43 @@ CREATE TABLE "employees" (
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar(20)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY ("emp_no", "dept_no")
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar(20)   NOT NULL
+    "dept_no" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "titles" (
-    "emp_title_id" varchar(10)   NOT NULL,
+    "title_id" varchar(10)   NOT NULL,
     "title" varchar(50)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
-        "emp_title_id"
+        "title_id"
      )
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("emp_title_id");
+REFERENCES "titles" ("title_id");
 
---ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
---REFERENCES "departments" ("dept_no");--
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -60,34 +70,38 @@ ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
 
+---Upload csv files
+
 COPY departments(dept_no, dept_name)
-FROM 'C:\data_csv\departments.csv'
+FROM 'C:\data\departments.csv'
 DELIMITER ','
 CSV HEADER;
 
 
 COPY dept_emp(emp_no, dept_no)
-FROM 'C:\data_csv\dept_emp.csv'
+FROM 'C:\data\dept_emp.csv'
 DELIMITER ','
 CSV HEADER;
 
 
 COPY dept_manager(dept_no, emp_no)
-FROM 'C:\data_csv\dept_manager.csv'
+FROM 'C:\data\dept_manager.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY employees(emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date)
-FROM 'C:\data_csv\employees.csv'
+FROM 'C:\data\employees.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY titles(emp_title_id, title)
-FROM 'C:\data_csv\titles.csv'
+COPY titles(title_id, title)
+FROM 'C:\data\titles.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY salaries(emp_no, salary)
-FROM 'C:\data_csv\salaries.csv'
+FROM 'C:\data\salaries.csv'
 DELIMITER ','
 CSV HEADER;
+
+
